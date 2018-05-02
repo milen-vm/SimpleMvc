761fd220c1ea5ed0e8f38db2a50d6f498e177a38
milen-vm <bay_milen@abv.bg>
2018-05-02 23:06:26 +0300

    Added config class.

---
+++ src/Config.php
@@ -0,0 +1,48 @@
+<?php
+namespace SimpleMvc;
+
+class Config
+{
+
+    private static $instance = null;
+
+    private static $configs = [];
+
+    private function __construct($configs)
+    {
+        self::$configs = $configs;
+    }
+
+    /**
+     * @param array $configs
+     * @return \SimpleMvc\Config
+     */
+    public static function getInstance($configs = [])
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($configs);
+        }
+
+        return self::$instance;
+    }
+
+    public static function get($str)
+    {
+        $keys = explode('.', $str);
+        $result = self::$configs;
+
+        foreach ($keys as $key) {
+            if (!is_array($result)) {
+                throw new \InvalidArgumentException("The configs is not array. Key '{$key}' can not be found.");
+            }
+
+            if (!isset($result[$key])) {
+                throw new \InvalidArgumentException("Key '{$key}' not found in configuration array.");
+            }
+
+            $result = $result[$key];
+        }
+
+        return $result;
+    }
+}
