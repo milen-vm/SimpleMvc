167c4c6e7963fc2d193abb533e5e18ae86408df9
milen-vm <bay_milen@abv.bg>
2018-05-09 23:23:46 +0300

    Implemented Render method.

--- src/Core/Mvc/View.php
+++ src/Core/Mvc/View.php
@@ -10,16 +10,30 @@ class View
 
     const VIEWS_DIR = VIEWS_DIR;
 
+    const LAYOUT_DIR = 'layouts';
+
+    const LAYOUT_HEADER = 'header';
+
+    const LAYOUT_FOOTER = 'footer';
+
+    const LAYOUT_DEFAULT = 'default';
+
     const FILE_EXTENSION = '.php';
 
     private static $app;
 
+    private $model;
+
     private $path;
+
+    private $layoutDir;
 
     public function __construct($model = null, $path = null)
     {
         self::$app = App::getInstance();
+        $this->model = $model;
         $this->setPath($path);
+        $this->layoutDir = self::VIEWS_DIR . self::LAYOUT_DIR . self::DS;
     }
 
     private function setPath($path)
@@ -31,7 +45,7 @@ class View
             $path = str_replace('/', self::DS, $path);
             $path = self::VIEWS_DIR . trim($path, self::DS)  . self::FILE_EXTENSION;
 
-            if (!file_exists($path)) {
+            if (!is_readable($path)) {
                 throw new \Exception("Template file is not found in the path {$path}");
             }
         }
@@ -47,8 +61,32 @@ class View
         return strtolower($path);
     }
 
-    public function render($hasLayout = true, $layout = 'default')
+    public function render($renderLayout = true, $layout = self::LAYOUT_DEFAULT)
     {
+        ob_start();
+        $model = $this->model;
+        if ($renderLayout) {
+            $header = $this->layoutDir . $layout . self::DS . self::LAYOUT_HEADER . self::FILE_EXTENSION;
+
+            if (!is_readable($header)) {
+                throw new \Exception('Header layout file not found.');
+            }
+
+            require_once $header;
+        }
+
         require_once $this->path;
+
+        if ($renderLayout) {
+            $footer = $this->layoutDir . $layout . self::DS . self::LAYOUT_FOOTER . self::FILE_EXTENSION;
+
+            if (!is_readable($header)) {
+                throw new \Exception('Footer layout file not found.');
+            }
+
+            require_once $footer;
+        }
+
+        echo ob_get_clean();
     }
 }
